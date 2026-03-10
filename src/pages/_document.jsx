d26303ee4ba4d1c4e18492688d3517115b43a8d6
Veeravel-S <veeravel.igt@gmail.com>
2026-03-10 16:21:38 +0530

    gtm code added

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 
 export default function Document() {
@@ -27,13 +26,21 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
         />
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WBKXJ4SD');`,
+          }}
+        />
       </Head>
       <body>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TPTQHSQ8"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WBKXJ4SD"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
